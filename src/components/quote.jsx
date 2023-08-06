331c1cde4624343620ddc0ba9df22b0a34e1205d
Michael <michael.riordan81@outlook.com>
2023-08-06 07:37:43 -0700

    fix axios delete query on quote page

--- src/components/quote.jsx
+++ src/components/quote.jsx
@@ -79,10 +79,7 @@ export default function Quote() {
             } else if (value === 'yes' && appointment !== '') {
                 saveToSessionStorage();
                 axios.delete(`${import.meta.env.VITE_SERVER_DOMAIN}/deleteAppointment/${appointmentId}`)
-                const queryParams = {
-                    appointmentId: appointmentId,
-                }
-                history.push(`./appointment?${queryParams}`)
+                history.push(`./appointment`)
             } else {
                 return
             };
