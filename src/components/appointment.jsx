9498b1d130bfcbe09c1b0c725ddb61d4affc7cf4
Michael <michael.riordan81@outlook.com>
2023-08-06 07:15:09 -0700

    add console log to check appointments state

--- src/components/appointment.jsx
+++ src/components/appointment.jsx
@@ -18,7 +18,7 @@ export default function Appointment() {
     const [hoursForCurrentDay, setHoursForCurrentDay] = useState([]);
     const [blockedDates, setBlockedDates] = useState(null);
     const [blockedTimesForDate, setBlockedTimesForDate] = useState([]);
-    console.log(appointmentId);
+    console.log(appointments);
 
     const history = useHistory();
     
