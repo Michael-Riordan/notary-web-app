78be7bac3155553055f063ac6a9f1ae84ea96e43
Michael <michael.riordan81@outlook.com>
2023-08-21 08:26:14 -0700

    add chosen appointment for user to view in form

--- src/components/quote.jsx
+++ src/components/quote.jsx
@@ -477,6 +477,9 @@ ${numberInput === ''? '' : `Call/Text me at ${numberInput}`}`)
                         <label htmlFor='appointment-prompt' className='input-label appointment-prompt'>
                                 {appointment !== '' && typeof appointment !== 'object' ? 'Choose a Different Appointment?' : 'Choose an Appointment? (optional)'}
                         </label>
+                        <p>
+                            {appointment !== '' && typeof appointment !== 'object' ? {appointment} : ''}
+                        </p>
                         <div id='yes-no'>
                             <div id='yes-no-selector'>
                                 <label htmlFor='yes' className='yes-no-label yes'>
